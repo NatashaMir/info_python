c1e54f60e593ae5cc4acc5c4f4378efaae93fa7d
NatashaMir <nataliya.miroshnychenko@gmail.com>
2018-01-17 11:34:36 +0200

    Create Airport.py

---
+++ venv/Airport.py
@@ -0,0 +1,16 @@
+class Employee:
+  def __init__(self, position, rate):
+    self.position = position
+    self.rate = rate #salary in one hour
+    
+class Plane:
+  def __init__(self, model, seat, fuel_expense):
+    self.model = model
+    self.seat = seat
+    self.fuel_expense = fuel_expense
+    
+class Fuel:
+  def __init__(self, prise):
+    self.prise = prise
+    
+   
