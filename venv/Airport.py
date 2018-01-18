bdeb47edf7db978133e9bcbf98b2b42afed3493c
NatashaMir <nataliya.miroshnychenko@gmail.com>
2018-01-18 11:29:19 +0200

    Update Airport.py

--- venv/Airport.py
+++ venv/Airport.py
@@ -7,32 +7,61 @@ class Crew:
   def __init__(self):
     self.employees = []
     
+  def add_employee(self, employee):
+    self.employees.append(employee)  
+    
 class Plane:
   def __init__(self, model, seat, fuel_expense):
     self.model = model
     self.seat = seat
     self.fuel_expense = fuel_expense
     
-class Fuel:
-  def __init__(self, prise):
-    self.prise = prise
-    
+
 class Flight:
-  def __init__(self, number, plane, distance, crew):
+   
+   tickets = 0
+  
+  def __init__(self, number, plane, distance, crew, ticket_cost):
     self.number = number
+    self.plane = plane
     self.distance = distance
     self.crew = crew
+    self.ticket_cost = ticket_cost
   
-  def fuel_expense(self):
-    return plane.fuel_expense * distance
+
+  def sale_ticket(self, n):
+     if n <= plane.seat:
+       tickets += n
+      else:
+        print("Number of seats exceeded")
+        
+  def total_cost(self):
+    total=0
+      for employee in crew.employees:
+        total+=employees.rate
+    total = total + plane.fuel_expense * distance + tickets * ticket_cost     
+    return total   
+        
     
-    
-    
-class Ticket:
-  def __init__(self, flight, seat_number, prise):
-    self.flight = flight
-    self.seat_number = seat_number
-    self.prise = prise
-    
-class Airline:
-  def __init__(self, crew)
+
+Anna = Employee('stewardess', 100)
+Mari = Employee('stewardess', 100)
+Bob = Employee('pilot', 500)
+Jack = Employee('pilot', 400)
+
+crew = Crew()
+crew.add_employee(Anna)
+crew.add_employee(Bob)
+crew.add_employee(Jack)
+
+plane = Plane('Boing 134', 10, 3)
+
+fligh = Flight('RT76H', plane, 100, crew, 50)
+
+
+
+# class Ticket:
+#  def __init__(self, flight, seat_number, prise):
+#   self.flight = flight
+#    self.seat_number = seat_number
+#    self.prise = prise
