662937e1caf44db564bb8a88321bdb7e61bd810e
Gennadii Miroshnychenko <genamister@gmail.com>
2018-01-21 09:16:35 +0200

    commit

--- venv/Airport.py
+++ venv/Airport.py
@@ -1,49 +1,67 @@
 class Employee:
-  def __init__(self, position, rate):
-    self.position = position
-    self.rate = rate #salary in one hour
-    
+    def __init__(self, position, rate):
+        self.position = position
+        self.rate = rate  # salary in one hour
+
+
 class Crew:
-  def __init__(self):
-    self.employees = []
-    
-  def add_employee(self, employee):
-    self.employees.append(employee)  
-    
+    def __init__(self):
+        self.employees = []
+
+    def add_employee(self, employee):
+        self.employees.append(employee)
+
+
 class Plane:
-  def __init__(self, model, seat, fuel_expense):
-    self.model = model
-    self.seat = seat
-    self.fuel_expense = fuel_expense
-    
+    def __init__(self, model, seat, fuel_expense):
+        self.model = model
+        self.seat = seat
+        self.fuel_expense = fuel_expense
+
+
+class Ticket:
+    def __init__(self, price):
+        self.price = price
+
 
 class Flight:
-   
-  tickets = 0
-  
-  def __init__(self, number, plane, distance, crew, ticket_cost):
-    self.number = number
-    self.plane = plane
-    self.distance = distance
-    self.crew = crew
-    self.ticket_cost = ticket_cost
-  
 
-  def sale_ticket(self, n):
-    if n <= self.plane.seat:
-      Flight.tickets += n
-    else:
-        print("Number of seats exceeded")
-        
-  def total_cost(self):
-    total=0
-    #salary of all employees
-    for employee in self.crew.employees:
-      total+=employee.rate
-    total = total + self.plane.fuel_expense * self.distance + Flight.tickets * self.ticket_cost   
-    return total   
-        
-    
+    def __init__(self, number, plane, distance, crew, ticket_cost):
+        self.number = number
+        self.plane = plane
+        self.distance = distance
+        self.crew = crew
+        self.ticket_cost = ticket_cost
+        self.tickets = []
+
+    def add_ticket(self, ticket):
+        if len(self.tickets) <= self.plane.seat:
+            self.tickets.append(ticket)
+            return True
+        else:
+            return False
+
+    def profit(self):
+        total = 0
+        # salary of all employees
+        for employee in self.crew.employees:
+            total -= employee.rate
+        for ticket in self.tickets:
+            total += ticket.price
+        total = total - self.plane.fuel_expense * self.distance * 200
+        return total
+
+
+class Airline:
+    def __init__(self):
+        self.flights = []
+
+    def profit(self):
+        total = 0
+        for fligth in self.flights:
+            total += fligth.profit
+        return total
+
 
 Anna = Employee('stewardess', 100)
 Mari = Employee('stewardess', 100)
@@ -63,4 +81,4 @@ fligh.sale_ticket(10)
 
 print(fligh.total_cost())
 
-  
+
